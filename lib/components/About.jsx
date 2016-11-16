47b2e28426b0357ae442b50b7b3f9f89e652c4ac
MilkMan90 <mlkaufman747@gmail.com>
2016-11-15 20:15:58 -0700

    timeline styled

--- lib/components/About.jsx
+++ lib/components/About.jsx
@@ -20,26 +20,39 @@ export default class About extends Component {
       <ul>
         <li>
           <div>
-            <time>2016</time>
-            Turing School
+            <time>August 2016 </time>
+            <p>Turing School of Software and Design
+            Front End Engineering Program
+            Denver, CO</p>
           </div>
         </li>
         <li>
           <div>
-            <time>2016</time>
-            Turing School
+            <time>October 2014</time>
+            <p>Controls Software Applications Engineer</p>
+            <p>Progressive Machine and Design</p>
+            <p>Rochester, NY</p>
           </div>
         </li>
         <li>
           <div>
-            <time>2016</time>
-            Turing School
+            <time>May 2013</time>
+            <p>Automation Engineer</p>
+            <p>Distech Systems</p>
+            <p>Rochester, NY</p>
           </div>
         </li>
         <li>
           <div>
-            <time>2016</time>
-            Turing School
+            <time>May 2013</time>
+            <p>Graduated from Binghamton University - State University of New York</p>
+            <p>Bachelor of Science in Computer Engineering</p>
+          </div>
+        </li>
+        <li>
+          <div>
+            <time>June 2008</time>
+            <p>Graduated from Honeoye Falls Lima High School</p>
           </div>
         </li>
       </ul>
