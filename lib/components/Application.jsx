64a9bed1a3f42e5ea5c1f3ed3a4a881981d69498
MilkMan90 <mlkaufman747@gmail.com>
2016-11-15 17:04:19 -0700

    latest

--- lib/components/Application.jsx
+++ lib/components/Application.jsx
@@ -41,11 +41,11 @@ export default class Application extends Component {
           </div>
 
           <div id='footerlinks'>
-                  <a href="mailto:matthew.leo.kaufman@gmail.com?Subject=IveBeenLookingForYou"><img src="./lib/imgs/icons/mail.svg" alt="mail" /></a>
-                  <a href="https://github.com/MilkMan90"><img src="./lib/imgs/icons/github.svg" alt="github" /></a>
+                  <a href="mailto:matthew.leo.kaufman@gmail.com?Subject=IveBeenLookingForYou" target="_blank"><img src="./lib/imgs/icons/mail.svg" alt="mail" /></a>
+                  <a href="https://github.com/MilkMan90" target="_blank"><img src="./lib/imgs/icons/github.svg" alt="github" /></a>
                   {/* <a href="https://www.facebook.com/matthewleokaufman"><img src="./lib/imgs/icons/facebook.svg" alt="facebook" /></a> */}
-                  <a href="https://twitter.com/theKaufMan?lang=en"><img src="./lib/imgs/icons/twitter.svg" alt="twitter" /></a>
-                  <a href="https://www.linkedin.com/in/matthew-kaufman-6725b734"><img src="./lib/imgs/icons/linkedin2.png" alt="linkedin" /></a>
+                  <a href="https://twitter.com/theKaufMan?lang=en" target="_blank"><img src="./lib/imgs/icons/twitter.svg" alt="twitter" /></a>
+                  <a href="https://www.linkedin.com/in/matthew-kaufman-6725b734" target="_blank"><img src="./lib/imgs/icons/linkedin2.png" alt="linkedin" /></a>
           </div>
         </nav>
 
